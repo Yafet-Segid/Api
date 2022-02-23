9f1b85447f0c0e6ebf97ce7757eee6310e970673
Your Name <you@example.com>
2022-02-23 15:05:39 -0600

    add js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// aergasdg
+
+
+
+async function start(){
+    const response = await fetch(' https://dog.ceo/api/breeds/list/all')
+    const data = await response.json()
+    console.log(data)
+    createBreadList(data.message)
+
+}
+
+start()
+
+
+function createBreadList(breedList){
+document.getElementById('breed').innerHTML = ` 
+ <select >
+<option >Choose a dog breed</option>
+${Object.keys(breedList).map(function(breed){
+    return ` <option>${breed}</option>`
+}).json('')}
+
+</select>`
+
+}
